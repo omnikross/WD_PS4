f89623b79477ea1ae32b93ce6e23bcf2132518b0
Maksim Byakov <bms1200@gmail.com>
2017-05-31 10:16:59 +0300

    Переделанный скрипт: новая запись в файл

--- mainTask/vote.php
+++ mainTask/vote.php
@@ -1,22 +1,38 @@
-<?php
-$vote = $_REQUEST["vote"];
+<?php 
+$vote = $_POST['vote'];
 
 $filename = "data.json";
-$content = file($filename);
+$content = json_decode(file_get_contents($filename), true) ?: 
+    ['Good' => 0, 'Bad' => 0];
 
-$array = explode(",", $content[0]);
-$good = isset($array[0]) ? $array[0] : 0;
-$bad = isset($array[1]) ? $array[1] : 0;
 if ($vote == 0) {
-  $good += 1;
+	$content['Good'] += 1;
 }
 if ($vote == 1) {
-  $bad += 1;
-}	
-
-$insertvote = $good . "," . $bad;
+	$content['Bad'] += 1;
+}
+ 
+$pretty = json_encode($content, JSON_PRETTY_PRINT);
 $fp = fopen($filename,"w");
-fputs($fp,$insertvote);
+fputs($fp,$pretty);
 fclose($fp);
-echo (json_encode(array($good, $bad)));
 ?>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+	google.charts.load("current", {packages:["corechart"]});
+	google.charts.setOnLoadCallback(drawChart);
+	function drawChart() {
+		var data = google.visualization.arrayToDataTable([
+			['Task', 'Percentage'],
+			['Good', <?php echo json_encode($content['Good']); ?>],
+			['Bad', <?php echo json_encode($content['Bad']); ?>]
+			]);
+		var options = {
+			title: 'We are: ',
+		};
+
+		var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+		chart.draw(data, options);
+	} 
+</script>
+<div id="piechart" style="width: 900px; height: 500px;"></div>
